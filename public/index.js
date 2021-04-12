3dbe52018b16a47419664c8e669f65732e2be08c
Hunter Lacefield <lace1010@gmail.com>
2021-04-12 14:23:05 +0800

    finished skillSection

--- public/index.js
+++ public/index.js
@@ -1,18 +1,94 @@
 console.log("connected");
 
+// This handles the navbar hover effect
+$("#navIndicator").css({
+  width: $("#navbarLinkSelected").width() + 16,
+  "margin-left": parseInt($("#navbarLinkSelected").css("margin-left")) - 8,
+  left: $("#navbarLinkSelected").position().left,
+});
+
+// Handles when a navbar option is hovered on
+$("nav a").hover(function () {
+  var position = $(this).position();
+  $("#navIndicator").css({
+    width: $(this).width() + 16,
+    "margin-left": parseInt($(this).css("margin-left")) - 8,
+    left: position.left,
+    transition: "all ease 0.6s",
+  });
+});
+
+// handles when the mouse leaves navbar and want to return to selected section. notice we only use this when we leave "nav" and not "nav a"
+$("nav").mouseleave(() => {
+  $("#navIndicator").css({
+    width: $("#navbarLinkSelected").width() + 16,
+    "margin-left": parseInt($("#navbarLinkSelected").css("margin-left")) - 8,
+    left: $("#navbarLinkSelected").position().left,
+  });
+});
+
 // color theme button handlers
 $("#colorThemeButton").click(() => {
   $("#tooltipContainer").css({ padding: "1rem", width: "15rem" });
+  $("#tooltipHeader").append(
+    "<span class='colorHeader'>CHOOSE COLOR <i id='tooltipExit' class='fas fa-times-circle' style='margin-left: 1rem'></i></span>"
+  );
 });
 
-$("#tooltipExit").click(() => {
+// need to use onclick here because we are working with an appended element
+$(document).on("click", "#tooltipExit", () => {
   $("#tooltipContainer").css({ padding: "0", width: "0" });
+  $(".colorHeader").remove();
 });
 
-$("#blueColorTheme").click(() => {
-  alert("pushed");
+// Changing theme colors
+let doc = document.documentElement;
+$("#lightBlueColorTheme").click(() => {
+  doc.style.setProperty("--navbar-color", "rgba(51, 109, 177, 1)");
+  doc.style.setProperty("--theme-color", "rgb(74, 174, 255)");
+  doc.style.setProperty("--icon-color-hover", "rgb(33, 71, 114)");
+
+  $("#lightBlueColorTheme").addClass("selected");
+  $("#greenColorTheme").removeClass("selected");
+  $("#redColorTheme").removeClass("selected");
+  $("#blueColorTheme").removeClass("selected");
 });
 
+// Changing to green color theme
 $("#greenColorTheme").click(() => {
-  alert("pushed");
+  doc.style.setProperty("--navbar-color", "green");
+  doc.style.setProperty("--theme-color", "lightgreen");
+  doc.style.setProperty("--icon-color-hover", "darkgreen");
+
+  $("#greenColorTheme").addClass("selected");
+  $("#lightBlueColorTheme").removeClass("selected");
+  $("#redColorTheme").removeClass("selected");
+  $("#blueColorTheme").removeClass("selected");
+});
+
+//Changing to red color theme
+$("#redColorTheme").click(() => {
+  document.documentElement.style.setProperty("--navbar-color", "red");
+  document.documentElement.style.setProperty(
+    "--theme-color",
+    "rgb(255, 147, 147)"
+  );
+  document.documentElement.style.setProperty("--icon-color-hover", "darkred");
+
+  $("#redColorTheme").addClass("selected");
+  $("#lightBlueColorTheme").removeClass("selected");
+  $("#greenColorTheme").removeClass("selected");
+  $("#blueColorTheme").removeClass("selected");
+});
+
+//Changing to navy color theme
+$("#blueColorTheme").click(() => {
+  document.documentElement.style.setProperty("--navbar-color", "navy");
+  document.documentElement.style.setProperty("--theme-color", "blue");
+  document.documentElement.style.setProperty("--icon-color-hover", "darkgrey");
+
+  $("#blueColorTheme").addClass("selected");
+  $("#lightBlueColorTheme").removeClass("selected");
+  $("#greenColorTheme").removeClass("selected");
+  $("#redColorTheme").removeClass("selected");
 });
