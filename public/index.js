603401ad7f37e08525cee574e5bc4e030870278b
Hunter Lacefield <lace1010@gmail.com>
2021-04-19 17:20:16 +0800

    Finsished rough draft of portfolio

--- public/index.js
+++ public/index.js
@@ -69,51 +69,132 @@ $(document).on("click", "#tooltipExit", () => {
 
 // Changing theme colors
 let doc = document.documentElement;
-$("#lightBlueColorTheme").click(() => {
+// Changing to colorTheme1
+$("#colorTheme1").click(() => {
+  doc.style.setProperty("--navbar-color", "rgb(53, 73, 83)");
+  doc.style.setProperty("--theme-color", "rgb(86, 119, 135)");
+  doc.style.setProperty("--theme-color-overlay", "rgba(86, 119, 135, .9)");
+
+  $("#colorTheme1").addClass("selected");
+  $("#colorTheme2").removeClass("selected");
+  $("#colorTheme3").removeClass("selected");
+  $("#colorTheme4").removeClass("selected");
+  $("#colorTheme5").removeClass("selected");
+  $("#colorTheme6").removeClass("selected");
+  $("#colorTheme7").removeClass("selected");
+  $("#colorTheme8").removeClass("selected");
+});
+
+// Changing to colorTheme2
+$("#colorTheme2").click(() => {
+  doc.style.setProperty("--navbar-color", "rgb(77, 72, 83)");
+  doc.style.setProperty("--theme-color", "rgb(117, 110, 126)");
+  doc.style.setProperty("--theme-color-overlay", "rgba(117, 110, 126, .9)");
+
+  $("#colorTheme2").addClass("selected");
+  $("#colorTheme1").removeClass("selected");
+  $("#colorTheme3").removeClass("selected");
+  $("#colorTheme4").removeClass("selected");
+  $("#colorTheme5").removeClass("selected");
+  $("#colorTheme6").removeClass("selected");
+  $("#colorTheme7").removeClass("selected");
+  $("#colorTheme8").removeClass("selected");
+});
+
+// Changing to colorTheme3
+$("#colorTheme3").click(() => {
+  doc.style.setProperty("--navbar-color", "rgb(71, 70, 114)");
+  doc.style.setProperty("--theme-color", "rgb(111, 109, 178)");
+  doc.style.setProperty("--theme-color-overlay", "rgba(111, 109, 178, .9)");
+
+  $("#colorTheme3").addClass("selected");
+  $("#colorTheme1").removeClass("selected");
+  $("#colorTheme2").removeClass("selected");
+  $("#colorTheme4").removeClass("selected");
+  $("#colorTheme5").removeClass("selected");
+  $("#colorTheme6").removeClass("selected");
+  $("#colorTheme7").removeClass("selected");
+  $("#colorTheme8").removeClass("selected");
+});
+
+// Changing to colorTheme4
+$("#colorTheme4").click(() => {
+  doc.style.setProperty("--navbar-color", "rgb(0, 128, 128)");
+  doc.style.setProperty("--theme-color", "rgb(4, 196, 196)");
+  doc.style.setProperty("--theme-color-overlay", "rgba(4, 196, 196, .9)");
+
+  $("#colorTheme4").addClass("selected");
+  $("#colorTheme1").removeClass("selected");
+  $("#colorTheme2").removeClass("selected");
+  $("#colorTheme3").removeClass("selected");
+  $("#colorTheme5").removeClass("selected");
+  $("#colorTheme6").removeClass("selected");
+  $("#colorTheme7").removeClass("selected");
+  $("#colorTheme8").removeClass("selected");
+});
+
+// Changing to colorTheme5
+$("#colorTheme5").click(() => {
+  doc.style.setProperty("--navbar-color", "rgb(102, 53, 76)");
+  doc.style.setProperty("--theme-color", "rgb(160, 83, 119)");
+  doc.style.setProperty("--theme-color-overlay", "rgba(160, 83, 119, .9)");
+
+  $("#colorTheme5").addClass("selected");
+  $("#colorTheme1").removeClass("selected");
+  $("#colorTheme2").removeClass("selected");
+  $("#colorTheme3").removeClass("selected");
+  $("#colorTheme4").removeClass("selected");
+  $("#colorTheme6").removeClass("selected");
+  $("#colorTheme7").removeClass("selected");
+  $("#colorTheme8").removeClass("selected");
+});
+
+// Changing to colorTheme6
+$("#colorTheme6").click(() => {
   doc.style.setProperty("--navbar-color", "rgba(51, 109, 177, 1)");
   doc.style.setProperty("--theme-color", "rgb(74, 174, 255)");
   doc.style.setProperty("--theme-color-overlay", "rgba(51, 139, 255, .9)");
 
-  $("#lightBlueColorTheme").addClass("selected");
-  $("#greenColorTheme").removeClass("selected");
-  $("#redColorTheme").removeClass("selected");
-  $("#blueColorTheme").removeClass("selected");
+  $("#colorTheme6").addClass("selected");
+  $("#colorTheme1").removeClass("selected");
+  $("#colorTheme2").removeClass("selected");
+  $("#colorTheme3").removeClass("selected");
+  $("#colorTheme4").removeClass("selected");
+  $("#colorTheme5").removeClass("selected");
+  $("#colorTheme7").removeClass("selected");
+  $("#colorTheme8").removeClass("selected");
 });
 
-// Changing to green color theme
-$("#greenColorTheme").click(() => {
-  doc.style.setProperty("--navbar-color", "green");
-  doc.style.setProperty("--theme-color", "lightgreen");
-  doc.style.setProperty("--theme-color-overlay", "rgba(144, 238, 144, .9)");
+// Changing to colorTheme7
+$("#colorTheme7").click(() => {
+  doc.style.setProperty("--navbar-color", "rgb(40, 116, 88)");
+  doc.style.setProperty("--theme-color", "rgb(61, 174, 132)");
+  doc.style.setProperty("--theme-color-overlay", "rgba(61, 174, 132, .9)");
 
-  $("#greenColorTheme").addClass("selected");
-  $("#lightBlueColorTheme").removeClass("selected");
-  $("#redColorTheme").removeClass("selected");
-  $("#blueColorTheme").removeClass("selected");
+  $("#colorTheme7").addClass("selected");
+  $("#colorTheme1").removeClass("selected");
+  $("#colorTheme2").removeClass("selected");
+  $("#colorTheme3").removeClass("selected");
+  $("#colorTheme4").removeClass("selected");
+  $("#colorTheme5").removeClass("selected");
+  $("#colorTheme6").removeClass("selected");
+  $("#colorTheme8").removeClass("selected");
 });
 
-//Changing to red color theme
-$("#redColorTheme").click(() => {
-  doc.style.setProperty("--navbar-color", "red");
-  doc.style.setProperty("--theme-color", "rgb(255, 147, 147)");
-  doc.style.setProperty("--theme-color-overlay", "rgb(255, 147, 147, .9)");
+// Changing to colorTheme8
+$("#colorTheme8").click(() => {
+  doc.style.setProperty("--navbar-color", "rgb(42, 82, 152)");
+  doc.style.setProperty("--theme-color", "rgb(58, 112, 206)");
+  doc.style.setProperty("--theme-color-overlay", "rgba(58, 112, 206, .9)");
 
-  $("#redColorTheme").addClass("selected");
-  $("#lightBlueColorTheme").removeClass("selected");
-  $("#greenColorTheme").removeClass("selected");
-  $("#blueColorTheme").removeClass("selected");
-});
-
-//Changing to navy color theme
-$("#blueColorTheme").click(() => {
-  doc.style.setProperty("--navbar-color", "navy");
-  doc.style.setProperty("--theme-color", "blue");
-  doc.style.setProperty("--theme-color-overlay", "rgb(0, 0, 255, .8)");
-
-  $("#blueColorTheme").addClass("selected");
-  $("#lightBlueColorTheme").removeClass("selected");
-  $("#greenColorTheme").removeClass("selected");
-  $("#redColorTheme").removeClass("selected");
+  $("#colorTheme8").addClass("selected");
+  $("#colorTheme1").removeClass("selected");
+  $("#colorTheme2").removeClass("selected");
+  $("#colorTheme3").removeClass("selected");
+  $("#colorTheme4").removeClass("selected");
+  $("#colorTheme5").removeClass("selected");
+  $("#colorTheme6").removeClass("selected");
+  $("#colorTheme7").removeClass("selected");
 });
 
 // testimonial owl carousel
