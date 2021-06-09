60889e664d42b40f556b2942788611e87197367a
Hunter Lacefield <lace1010@gmail.com>
2021-06-09 17:56:04 +0800

    changed nodemailer create transport section so contact page works on server side and local host

--- server.js
+++ server.js
@@ -53,7 +53,9 @@ app.post("/email", (req, res) => {
 
   // The email that is sending the form info to my email
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
